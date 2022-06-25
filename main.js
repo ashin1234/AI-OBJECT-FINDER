0820b2ecc5e1b24c176d82ef7e0f923905c9671a
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-06-24 20:31:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+status = "";
+
 function preload(){
 
 }
 function setup(){
-canvas= createCanvas(350, 350);
+canvas= createCanvas(480, 380);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(480, 380);
+video.hide();
 }
 function draw(){
-
+image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object = document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
 }
